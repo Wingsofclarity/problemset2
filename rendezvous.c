2b30e9a37b65103066ca16577ea3ac6b3d86bd7e
Raymond Hammarling <raymond@hammarlings.se>
2016-03-31 20:49:26 +0200

    Implement rendezvous

--- rendezvous.c
+++ rendezvous.c
@@ -16,12 +16,15 @@
 
 #define LOOPS 5
 #define NTHREADS 3
-#define MAX_SLEEP_TIME 3
+#define MAX_SLEEP_TIME 12
 
 
 /* TODO: Make the two threads perform their iterations in a
  * predictable way. Both should perform iteration 1 before iteration 2
  * and then 2 before 3 etc. */
+
+sem_t semA;
+sem_t semB;
 
 void *
 threadA(void *param __attribute__((unused)))
@@ -29,10 +32,12 @@ threadA(void *param __attribute__((unused)))
     int i;
 
     for (i = 0; i < LOOPS; i++) {
+        sem_wait(&semA);
 
-	printf("threadA --> %d iteration\n", i);
-	sleep(rand() % MAX_SLEEP_TIME);
+    	printf("threadA --> %d iteration\n", i);
+    	sleep(rand() % MAX_SLEEP_TIME);
 
+        sem_post(&semB);
     }
 
     pthread_exit(0);
@@ -45,11 +50,13 @@ threadB(void *param  __attribute__((unused)))
     int i;
 
     for (i = 0; i < LOOPS; i++) {
+        sem_wait(&semB);
 
+    	printf("threadB --> %d iteration\n", i);
+    	// sleep(rand() % MAX_SLEEP_TIME);
+        sleep(i);
 
-	printf("threadB --> %d iteration\n", i);
-	sleep(rand() % MAX_SLEEP_TIME);
-
+        sem_post(&semA);
     }
 
     pthread_exit(0);
@@ -59,19 +66,21 @@ int
 main()
 {
     pthread_t tidA, tidB;
+    sem_init(&semA, 0, 1);
+    sem_init(&semB, 0, 1);
 
     srand(time(NULL));
     pthread_setconcurrency(3);
 
     if (pthread_create(&tidA, NULL, threadA, NULL) ||
-	pthread_create(&tidB, NULL, threadB, NULL)) {
-	perror("pthread_create");
-	abort();
+    	pthread_create(&tidB, NULL, threadB, NULL)) {
+    	perror("pthread_create");
+    	abort();
     }
     if (pthread_join(tidA, NULL) != 0 ||
         pthread_join(tidB, NULL) != 0) {
-	perror("pthread_join");
-	abort();
+    	perror("pthread_join");
+    	abort();
     }
 
     return 0;
